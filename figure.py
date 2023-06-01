77aeaa81bdb4fb32321c9cda5c11167ea8fc6431
Seonye32 <bsy040426@gmail.com>
2023-06-01 14:35:54 +0900

    implement hw5

--- figure.py
+++ figure.py
@@ -2,10 +2,13 @@ import math
 
 class line:
     """
-    선의 길이와 관련된 class는 line임
-    외부에서는 접근 불가능한 __width 와 __height 를 설정함
-    해당 변수를 수정하기 위해
-    set_length 와 get_length 를 제공함
+    클래스에 대한 설명은 class의 선언 바로 아래에 위치합니다.
+    
+    line class는 선의 길이에 대해 저장하고 있는 클래스입니다.
+    변수로는 외부에서 접근 불가능한 __width와 __height가 있으며,
+    해당 변수를 수정하고 접근하기 위해
+    set_length와, get_length 메소드를 제공합니다.
+
     """
     __width = 0
     __height = 0
@@ -13,52 +16,64 @@ class line:
 
     def __init__(self, width, height):
         """
-        초기의 width값과 height값을 받음
-        witdh -> 초기 선의 가로 길이 
-        height -> 초기 선의 세로 길이
+        생성자 초기 width와 height 값을 입력
+        Args:
+            width (int or float): 초기 선의 가로 길이
+            height (int or float): 초기 선의 세로 길이
         """
         self.__width = width
         self.__height = height
 
     def set_length(self, width, height):
         """
-        선의 길이 설정
-        width -> 수정할 가로의 길이
-        height -> 수정할 세로의 길이
+        선의 길이를 수정
+        Args:
+            width (int or float): 수정하고자 하는 가로 길이
+            height (int or float): 수정하고자 하는 세로 길이
         """
         self.__width = width
         self.__height = height
 
     def get_length(self):
         """
-        객체가 저장하고 있는 선의 길이 반환
-        리턴값: 저장된 선의 길이
+        객체가 저장하고 있는 선의 길이를 반환
+        Returns:
+            int or float: 저장하고 있는 선의 길이
         """
         return self.__width, self.__height
 
 def area_rectangle(width, height):
     """
-    길이를 입력 받아서 직사각형의 넓이를 구하는 함수
-    인자: width-> 가로의 길이, height -> 세로의 길이
-    리턴값: 작사각형의 넓이
+    길이를 입력받아 직사각형의 넓이를 구하는 함수
+        Args:
+            width (int or float): 밑변의 길이
+            height (int or float): 높이의 길이
+        Returns:
+            int or float: 직사각형의 넓이를 반환
     """
     if width <= 0 or height <= 0: raise ValueError
     return width * height
 
 def area_ellipse(width, height):
     """
-    길이를 입력 받아서 타원의 넓이를 구하는 함수
-    인자: width -> 짧은쪽 반지름의 길이, height -> 긴쪽 반지름의 길이
-    리턴값: 타원의 넓이
+     길이를 입력받아 타원의 넓이를 구하는 함수
+        Args: 
+            width (int or float): 짧은쪽 반지름 길이
+            height (int or float): 긴쪽 반지름 길이
+        Returns:
+            int or float : 원의 넓이를 반환
     """
     if width <= 0 or height <= 0: raise ValueError
     return width * height * math.pi
 
 def area_right_triangle(width, height):
     """
-    길이를 입력 받아서 정삼각형의 넓이를 구하는 함수
-    인자: width -> 밑 변의 길이, height -> 높이의 길이
-    리턴값: 직각삼각형의 넓이
+    길이를 입력받아 직각삼각형의 넓이를 구하는 함수
+        Args: 
+            width (int or float): 밑변의 길이
+            height (int or float): 높이의 길이
+        Returns:
+            int or float : 직각삼각형의 넓이를 반환
     """
     if width <= 0 or height <= 0: raise ValueError
     return width * height / 2
--- figure_main.py
+++ figure_main.py
@@ -3,18 +3,8 @@ import figure
 myline = figure.line(10, 20)
 
 width, height = myline.get_length()
-# implement exception handler
-rectangle = figure.area_rectangle(width, height)
-print(rectangle)
-
-myline.set_length(20, 30)
-width, height = myline.get_length()
-# implement exception handler
-triangle = figure.area_right_triangle(width, height)
-print(triangle)
-
-myline.set_length(30, 40)
-width, height = myline.get_length()
-# implement exception handler
-ellipse = figure.area_ellipse(width, height)
-print(ellipse)
+try:
+    rectangle = figure.area_rectangle(width, height)
+    print(rectangle)
+except ValueError:
+    print("please input positive number for width and height")
